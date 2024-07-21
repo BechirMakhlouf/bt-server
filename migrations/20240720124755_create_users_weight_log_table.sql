e27812e14c0743264781a6a1eca4d7300ba084e5
bechirMakhlouf <bechirM@proton.me>
2024-07-21 21:36:28 +0100

    add measurements tables

--- migrations/20240720124755_create_users_weight_log_table.sql
+++ migrations/20240720124755_create_users_weight_log_table.sql
@@ -1,10 +1,8 @@
 -- Add migration script here
 create table users_weight_log (
-  date_at DATE NOT NULL CHECK(date_at <= CURRENT_DATE),
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
   user_id UUID NOT NULL,
-  weight_kg FLOAT NOT NULL CHECK(weight_kg between 5 and 1000),
-  CONSTRAINT user_id_fk
-    FOREIGN KEY (user_id) REFERENCES users(id),
-  CONSTRAINT pkey
-    PRIMARY KEY (user_id, date_at)
+  weight_kg FLOAT NOT NULL CHECK (weight_kg between 5 and 1000),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- migrations/20240721164137_create_user_body_measurements_cm_table.sql
+++ migrations/20240721164137_create_user_body_measurements_cm_table.sql
@@ -1,23 +1,76 @@
 -- Add migration script here
-create table user_body_measurements_cm(
-  id            uuid    NOT NULL   DEFAULT gen_random_uuid(),
-  user_id       uuid    NOT NULL,
-  height        float              CHECK(height between 50 and 300), 
-  left_arm      float              CHECK(arms between 50 and 200),
-  right_arm     float              CHECK(arms between 50 and 200),
-  thighs        float              CHECK(thighs between 20 and 300),
-  left_wrist    float              CHECK(wrist between 5 and 100),
-  right_wrist   float              CHECK(wrist between 5 and 100),
-  neck          float              CHECK(neck between 10 and 50),
-  left_calf     float              CHECK(calves between 20 and 100),
-  right_calf    float              CHECK(calves between 20 and 100),
-  hips          float              CHECK(hips between 50 and 400),
-  torso         float              CHECK(torso between 50 and 400),
-  waist         float              CHECK(waist between 50 and 400),
-  date_at       date    NOT NULL   CHECK(date_at <= CURRENT_DATE),
+create table height (
+  height FLOAT CHECK (height > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
 
-  CONSTRAINT pkey
-    PRIMARY KEY (id),
-  CONSTRAINT user_id_fk
-    FOREIGN KEY (user_id) REFERENCES users(id)
+create table thighs (
+  left_thigh FLOAT CHECK (left_thigh > 0),
+  right_thigh FLOAT CHECK (right_thigh > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create table neck (
+  neck FLOAT CHECK (neck > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create table waist (
+  waist FLOAT CHECK (waist > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create table torso (
+  torso FLOAT CHECK (torso > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create table hips (
+  hips FLOAT CHECK (hips > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create table calves (
+  left_calf FLOAT CHECK (left_calf > 0),
+  right_calf FLOAT CHECK (right_calf > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create table arms (
+  left_arm FLOAT CHECK (left_arm > 0),
+  right_arm FLOAT CHECK (right_arm > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create table wrists (
+  left_wrist float CHECK (left_wrist > 0),
+  right_wrist float CHECK (right_wrist > 0),
+  user_id UUID NOT NULL,
+  date_at DATE NOT NULL CHECK (date_at <= CURRENT_DATE),
+  PRIMARY KEY (user_id, date_at),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
 );
