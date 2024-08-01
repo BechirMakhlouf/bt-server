115bb9c5f49ea304c78d0cd9d5909e186d5767bd
bechirMakhlouf <bechirM@proton.me>
2024-08-01 14:59:12 +0100

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use actix_web::{App, HttpServer};
+use actix_web::{
+    web::{scope, Data},
+    App, HttpServer,
+};
 use configuration::ApplicationSettings;
 use env_logger::Env;
-use sqlx::Postgres;
-use std::net::TcpListener;
+use log::info;
+use models::user_settings;
+use repositories::Repositories;
+use services::AppState;
+use std::{net::TcpListener, sync::Arc};
 
+mod auth;
 mod configuration;
 mod models;
 mod repositories;
@@ -14,33 +21,46 @@ mod types;
 async fn main() -> std::io::Result<()> {
     let application_settings = ApplicationSettings::get_settings().unwrap();
 
-    let db_pool = application_settings
-        .database
-        .get_db_pool::<Postgres>()
-        .await
-        .unwrap();
-
-    let _body_measurements_repository = repositories::BodyMeasurementsRepository::new(&db_pool);
-    let _user_repository = repositories::UserRepository::new(&db_pool);
-    let _user_info_repository = repositories::UserInfoRepository::new(&db_pool);
-    let _user_weight_log_repository = repositories::UserWeightRepository::new(&db_pool);
-    let _user_body_fat_repository = repositories::UserBodyFatRepository::new(&db_pool);
+    let db_pool = Arc::new(
+        sqlx::postgres::PgPool::connect_lazy(application_settings.database.database_url.as_str())
+            .expect("failed to connect to postgres database"),
+    );
 
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    let addr = TcpListener::bind(format!("127.0.0.1:{}", application_settings.port))
-        .unwrap()
-        .local_addr()
-        .unwrap();
+    let addr = TcpListener::bind(format!(
+        "{}:{}",
+        application_settings.host, application_settings.port
+    ))
+    .unwrap()
+    .local_addr()
+    .unwrap();
 
-    println!("port: {}", addr.port());
+    info!("Server running on: {}", addr.to_string());
 
-    HttpServer::new(|| {
+    let app_state = AppState::new(Repositories::new(db_pool));
+
+    use services::*;
+    let _ = HttpServer::new(move || {
         App::new()
             .wrap(tracing_actix_web::TracingLogger::default())
+            .app_data(Data::new(app_state.clone()))
             .service(services::health::check)
+            .service(services::auth::get_scope("/auth"))
+            .service(
+                scope("/user")
+                    .service(user_profile::get_scope("/profile"))
+                    .service(user_info::get_scope("/info"))
+                    .service(user_settings::get_scope("/settings"))
+                    .service(user_weight::get_scope("/weight"))
+                    .service(user_body_fat::get_scope("/body-fat"))
+                    .service(user_body_measurements::get_scope("/body-measurements")),
+            )
     })
     .bind(addr)?
     .run()
-    .await
+    .await;
+
+    info!("Server closing.: {}", addr.to_string());
+    Ok(())
 }
